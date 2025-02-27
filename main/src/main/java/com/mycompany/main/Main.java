7bd7276acaf6e30f8c7531ccc00828b9ec1c6fd6
Danny <Danny@Otame-Laptop>
2025-02-27 10:43:19 -0500

    Día 6: Programa de gestión de bibliotecas

--- main/src/main/java/com/mycompany/main/Main.java
+++ main/src/main/java/com/mycompany/main/Main.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.main;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +12,37 @@ package com.mycompany.main;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        String eleccion = "";
+        while (!eleccion.equals("9")) {
+            System.out.println(
+                    "1. Registrar socio\n2. Registrar libro\n3. Registrar prestamo\n4. Ver socios\n5. Ver libros\n6. Ver prestamos\n7. Ver socios no fiables\n8. Cambiar localizacion de libro\n9. Salir\nElige:");
+            eleccion = sc.nextLine();
+            if (eleccion.equals("1")) {
+                ControladorSocios.solicitarDatosParaRegistrar();
+            }
+            if (eleccion.equals("2")) {
+                ControladorLibros.solicitarDatosParaRegistrar();
+            }
+            if (eleccion.equals("3")) {
+                ControladorPrestamos.solicitarDatosYCrearPrestamo();
+            }
+            if (eleccion.equals("4")) {
+                ControladorSocios.imprimirSocios(ControladorSocios.obtener());
+            }
+            if (eleccion.equals("5")) {
+                ControladorLibros.imprimirLibros(ControladorLibros.obtener());
+            }
+            if (eleccion.equals("6")) {
+                ControladorPrestamos.imprimirPrestamos(ControladorPrestamos.obtener());
+            }
+            if (eleccion.equals("7")) {
+                ControladorSocios.imprimirSociosNoFiables(ControladorSocios.obtener());
+            }
+            if (eleccion.equals("8")) {
+                ControladorLibros.solicitarDatosParaCambiarSignatura();
+            }
+
+        }
     }
 }
