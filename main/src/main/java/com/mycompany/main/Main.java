28cda667ac604fabf5bdbd7bd4643c8bedb18319
Danny <Danny@Otame-Laptop>
2025-02-27 10:27:21 -0500

    Día 5: Verificador de palíndromos

--- main/src/main/java/com/mycompany/main/Main.java
+++ main/src/main/java/com/mycompany/main/Main.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.main;
 
 /**
@@ -11,6 +10,20 @@ package com.mycompany.main;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String[] strings = {"La ruta natural", "Esto no es", "Parzibyte", "Hola", "Sol", "Ana", "Oro", "Oso",
+            "A ti no, bonita",
+            "Adivina ya te opina, ya ni miles origina, ya ni cetro me domina, ya ni monarcas, a repaso ni mulato carreta, acaso nicotina, ya ni cita vecino, anima cocina, pedazo gallina, cedazo terso nos retoza de canilla goza, de pánico camina, ónice vaticina, ya ni tocino saca, a terracota luminosa pera, sacra nómina y ánimo de mortecina, ya ni giros elimina, ya ni poeta, ya ni vida",
+            "A mamá, Roma le aviva el amor a papá y a papá, Roma le aviva el amor a Mamá", "Me gusta programar en Java"};
+        for (String string : strings) {
+            System.out.println("¿'" + string + "' es palíndromo? " + isPalindrome(string));
+        }
     }
+
+    public static boolean isPalindrome(String string) {
+        string = string.toLowerCase().replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o")
+                .replace("ú", "u").replace(" ", "").replace(".", "").replace(",", "");
+        String reverseString = new StringBuilder(string).reverse().toString();
+        return reverseString.equals(string);
+    }
+
 }
