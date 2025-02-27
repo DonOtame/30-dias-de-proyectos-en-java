5c52391f097c5987fa32520d87b730fb679c6b2c
Danny <Danny@Otame-Laptop>
2025-02-27 10:19:07 -0500

    Día 3: Juego de adivinanza de números

--- main/src/main/java/com/mycompany/main/Main.java
+++ main/src/main/java/com/mycompany/main/Main.java
@@ -1,8 +1,10 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.main;
+
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  *
@@ -11,6 +13,23 @@ package com.mycompany.main;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        int magicNumber = ThreadLocalRandom.current().nextInt(1, 100);
+        int userNumber;
+
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.println("Intenta adivinar el número que he pensado. Ingresa tu propuesta: ");
+            userNumber = scanner.nextInt();
+            if (magicNumber > userNumber) {
+                System.out.println("El número que he pensado es mayor que el introducido");
+            } else if (magicNumber < userNumber) {
+                System.out.println("El número que he pensado es menor que el introducido");
+            }
+        } while (magicNumber != userNumber);
+
+        System.out.println("Has acertado, el número que pensé fue: " + magicNumber);
+
     }
 }
